b79dfa741d5f17221c9ddb1928df63d6f5de115f
AnghelLeonard <leoprivacy@yahoo.com>
2018-11-17 09:01:22 +0200

    Add files via upload

---
+++ 8/quickstart/Dockerfile
@@ -0,0 +1,2 @@
+FROM airhacks/glassfish
+COPY ./target/quickstart.war ${DEPLOYMENT_DIR}
---
+++ 8/quickstart/buildAndRun.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mvn clean package && docker build -t com.sample/quickstart .
+docker rm -f quickstart || true && docker run -d -p 8080:8080 -p 4848:4848 --name quickstart com.sample/quickstart 
